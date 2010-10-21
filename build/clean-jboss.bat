d208e6bc486f07753ae54f6f7b9fe01bb23e6d6c
zoltan.kiss.jr@gmail.com <zoltan.kiss.jr@gmail.com@0be8cae3-71aa-c058-6ec9-67f785d71e5e>
2010-10-21 22:02:32 +0000

    build: bat-ok cserélése ant-ra

--- build/clean-jboss.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call setenv %*
-
-rd /S /Q %SERVER_DIR%\work
-rd /S /Q %SERVER_DIR%\tmp
-rd /S /Q %SERVER_DIR%\data
-rd /S /Q %SERVER_DIR%\log
--- build/run-jboss.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call setenv.bat
-
-title Series Portal JBoss server
-
-set LOCAL_IP=0.0.0.0
-
-"%JBOSS_DIR%\bin\debug.bat" -c %SERVER_NAME% -b%LOCAL_IP%
