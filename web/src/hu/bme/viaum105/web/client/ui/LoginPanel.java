d60c62bd0e701835b85ec8fbaff6bcff952040e1
zbalazs86 <zbalazs86@0be8cae3-71aa-c058-6ec9-67f785d71e5e>
2010-10-23 18:49:26 +0000

    Bejelentkező és regisztrációs formok nagyjából készek.

---
+++ web/src/hu/bme/viaum105/web/client/ui/LoginPanel.java
@@ -0,0 +1,59 @@
+package hu.bme.viaum105.web.client.ui;
+
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Bejelentkezés panel.
+ * 
+ * @author zoli
+ */
+public class LoginPanel extends VerticalPanel{
+	
+	Label loginNameLabel = new Label();
+	TextBox loginNameField = new TextBox();
+	Label passwordLabel = new Label();
+	PasswordTextBox passwordField = new PasswordTextBox();
+	
+	Label errorMessage = new Label(); 
+	
+	public LoginPanel() {
+		initComponents();
+	}
+	
+	/**
+	 * Komponensek inicializálása.
+	 */
+	public void initComponents() {
+		loginNameLabel.setText("Login name");
+		passwordLabel.setText("Password");
+		
+		errorMessage.setText("");
+		
+		Grid grid = new Grid(2, 2);
+		grid.setWidget(0, 0, loginNameLabel);
+		grid.setWidget(0, 1, loginNameField);
+		grid.setWidget(1, 0, passwordLabel);
+		grid.setWidget(1, 1, passwordField);
+		
+		add(grid);
+		add(errorMessage);
+		
+	}
+	
+	public String getLoginName() {
+		return loginNameField.getText();
+	}
+	
+	public String getPassword() {
+		return passwordField.getText();
+	}
+	
+	public void setErrorMessage(String message) {
+		errorMessage.setText(message);
+	}
+
+}
---
+++ web/src/hu/bme/viaum105/web/client/ui/RegisterPanel.java
@@ -0,0 +1,71 @@
+package hu.bme.viaum105.web.client.ui;
+
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Regisztráció panel.
+ * 
+ * @author zoli
+ */
+public class RegisterPanel extends VerticalPanel{
+	
+	private Label loginNameLabel = new Label();
+	private TextBox loginNameField = new TextBox();
+	private Label passwordLabel = new Label();
+	private PasswordTextBox passwordField = new PasswordTextBox();
+	private Label passwordRetypeLabel = new Label();
+	private PasswordTextBox passwordRetypeField = new PasswordTextBox();
+	private Label emailLabel = new Label();
+	private TextBox emailField = new TextBox();
+	
+	public RegisterPanel() {
+		initComponents();
+	}
+	
+	private void initComponents() {
+		loginNameLabel.setText("Login name");
+		passwordLabel.setText("Password");
+		passwordRetypeLabel.setText("Retype password");
+		emailLabel.setText("Email");
+		
+		Grid grid = new Grid(4, 2);
+		grid.setWidget(0, 0, loginNameLabel);
+		grid.setWidget(0, 1, loginNameField);
+		grid.setWidget(1, 0, passwordLabel);
+		grid.setWidget(1, 1, passwordField);
+		grid.setWidget(2, 0, passwordRetypeLabel);
+		grid.setWidget(2, 1, passwordRetypeField);
+		grid.setWidget(3, 0, emailLabel);
+		grid.setWidget(3, 1, emailField);
+		
+		add(grid);
+	}
+	
+	public String getLoginName() {
+		return loginNameField.getText();
+	}
+	
+	public String getPassword() {
+		return passwordField.getText();
+	}
+	
+	public String getEmail() {
+		return emailField.getText();
+	}
+	
+	public boolean isValid() {
+		
+		boolean loginValid = !getLoginName().isEmpty(); 
+		boolean passwordValid = passwordField.getText().equals(
+				passwordRetypeField.getText()) && !getPassword().isEmpty();
+		//TODO jobb implementáció
+		boolean emailValid = !getEmail().isEmpty();
+		
+		return (loginValid && passwordValid && emailValid);
+	}
+
+}
