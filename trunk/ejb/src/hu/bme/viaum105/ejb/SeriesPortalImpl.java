9ca6f9ab8ec6031471f4fd7163134975f9aa07a3
zoltan.kiss.jr@gmail.com <zoltan.kiss.jr@gmail.com@0be8cae3-71aa-c058-6ec9-67f785d71e5e>
2010-10-29 15:32:28 +0000

    üzleti logika

--- trunk/ejb/src/hu/bme/viaum105/ejb/SeriesPortalImpl.java
+++ trunk/ejb/src/hu/bme/viaum105/ejb/SeriesPortalImpl.java
@@ -1,5 +1,6 @@
 package hu.bme.viaum105.ejb;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +12,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
+import hu.bme.viaum105.data.persistent.Comment;
 import hu.bme.viaum105.data.persistent.Episode;
+import hu.bme.viaum105.data.persistent.Like;
+import hu.bme.viaum105.data.persistent.Rate;
 import hu.bme.viaum105.data.persistent.RegisteredEntity;
 import hu.bme.viaum105.data.persistent.Series;
 import hu.bme.viaum105.data.persistent.Subtitle;
@@ -35,12 +39,41 @@ public class SeriesPortalImpl implements SeriesPortal {
     private EntityManagerFactory entityManagerFactory;
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
-    public List<Series> getAllSeriesPaged(int pageSize, int pageNumber) throws DaoException {
-	SeriesPortalImpl.log.trace("getAllSeriesPaged");
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Comment comment(RegisteredEntity registeredEntity, User user, String comment) throws DaoException {
+	SeriesPortalImpl.log.trace("comment");
 	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 	try {
-	    return new SeriesPortalDao(entityManager).getAllSeriesPaged(pageSize, pageNumber);
+	    Comment ret = new Comment();
+	    ret.setComment(comment);
+	    ret.setDate(new Date());
+	    ret.setRegisteredEntity(registeredEntity);
+	    ret.setUser(user);
+	    return new SeriesPortalDao(entityManager).save(ret);
+	} finally {
+	    entityManager.close();
+	}
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public long getLikeCount(long registeredEntityId) throws DaoException {
+	SeriesPortalImpl.log.trace("getLikeCount");
+	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+	try {
+	    return new SeriesPortalDao(entityManager).getLikeCount(registeredEntityId);
+	} finally {
+	    entityManager.close();
+	}
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public double getRate(long registeredEntityId) throws DaoException {
+	SeriesPortalImpl.log.trace("getRate");
+	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+	try {
+	    return new SeriesPortalDao(entityManager).getRate(registeredEntityId);
 	} finally {
 	    entityManager.close();
 	}
@@ -54,11 +87,46 @@ public class SeriesPortalImpl implements SeriesPortal {
 
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
-    public void rate(RegisteredEntity registeredEntity, User user, int rate) throws DaoException {
+    public Like like(RegisteredEntity registeredEntity, User user) throws DaoException {
+	SeriesPortalImpl.log.trace("like");
 	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 	try {
-	    // TODO rate keresése, user értékelte-e már
-	    // Ha igen, módosítás, egyébként insert
+	    Like like = new Like();
+	    like.setRegisteredEntity(registeredEntity);
+	    like.setUser(user);
+	    return new SeriesPortalDao(entityManager).save(like);
+	} finally {
+	    entityManager.close();
+	}
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public List<Series> listSeriesPaged(int pageSize, int pageNumber) throws DaoException {
+	SeriesPortalImpl.log.trace("getAllSeriesPaged");
+	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+	try {
+	    return new SeriesPortalDao(entityManager).getAllSeriesPaged(pageSize, pageNumber);
+	} finally {
+	    entityManager.close();
+	}
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Rate rate(RegisteredEntity registeredEntity, User user, int rate) throws DaoException {
+	SeriesPortalImpl.log.trace("rate");
+	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+	try {
+	    SeriesPortalDao dao = new SeriesPortalDao(entityManager);
+	    Rate r = dao.findRate(registeredEntity, user);
+	    if (r == null) {
+		r = new Rate();
+		r.setUser(user);
+		r.setRegisteredEntity(registeredEntity);
+	    }
+	    r.setRate(rate);
+	    return dao.save(r);
 	} finally {
 	    entityManager.close();
 	}
@@ -81,6 +149,7 @@ public class SeriesPortalImpl implements SeriesPortal {
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Episode save(Episode episode) throws DaoException {
+	SeriesPortalImpl.log.trace("save episode");
 	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 	try {
 	    return new SeriesPortalDao(entityManager).save(episode);
@@ -92,6 +161,7 @@ public class SeriesPortalImpl implements SeriesPortal {
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Series save(Series series) throws DaoException {
+	SeriesPortalImpl.log.trace("save series");
 	EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 	try {
 	    return new SeriesPortalDao(entityManager).save(series);
@@ -103,6 +173,7 @@ public class SeriesPortalImpl implements SeriesPortal {
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Subtitle uploadSubtitle(Episode episode, Subtitle subtitle, SubtitleData subtitleData) throws DaoException, ServerException {
+	SeriesPortalImpl.log.trace("uploadSubtitle");
 	// TODO Auto-generated method stub
 	return null;
     }
